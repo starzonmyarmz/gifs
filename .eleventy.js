546616bf0b99cb726fdd58c379d14478738a7899
Daniel Marino <starzonmyarmz@gmail.com>
2021-01-22 10:38:36 -0500

    better api

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,10 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/images')
   eleventyConfig.addPassthroughCopy('src/_redirects')
 
+  eleventyConfig.addFilter('titlize', (val) => {
+    return val.replace(/-/g, ' ')
+  })
+
   return {
     dir: {
       input: 'src',
