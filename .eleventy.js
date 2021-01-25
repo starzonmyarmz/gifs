4dc3ec000d81dbbe3fa3a783fb72345d31747e78
Daniel Marino <starzonmyarmz@gmail.com>
2021-01-25 11:14:00 -0500

    Simplify structure

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('src/images')
-  eleventyConfig.addPassthroughCopy('src/_redirects')
+  eleventyConfig.addPassthroughCopy("g")
 
   eleventyConfig.addFilter('titlize', (val) => {
     return val.replace(/-/g, ' ')
--- src/_data/helpers.js
+++ src/_data/helpers.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 module.exports = {
   gifs(state, type) {
-    return glob.sync(`./src/images/*.{gif,jpg,png,webp}`)
+    return glob.sync(`./g/*.gif`)
       .map((gif) => {
         return path.parse(gif).name
       })
